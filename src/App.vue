dc04c3630e6913b2d872ae6348e91e7a3e67fb7a
Fabio <thebloodshadow14@gmail.com>
2023-11-10 14:34:23 +0100

    INIT - setup

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import NavBar from "./components/NavBar.vue";
+
+import { store } from "./store";
+import axios from "axios";
+
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      store,
+      apiUrlMovie: "https://api.themoviedb.org/3/search/movie?",
+      apiUrlTv: "https://api.themoviedb.org/3/search/tv?",
+      apiKey: "d306f972311cd14431a46f101f74adb9",
+    };
+  },
+  methods: {
+    fetchData: function (apiUrl, key, query) {
+      axios.get(apiUrl, { params: { api_key: key, query: query } }).then((resp) => console.log(resp));
+    },
+  },
+  beforeMount() {
+    this.fetchData(this.apiUrlTv, this.apiKey, this.store.query);
+  },
+};
+</script>
+
+<template>
+  <span class="bg-red-500">{{ store }}</span>
+  <NavBar />
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script>
+import { store } from "../store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <input type="text" @keyup.enter="" v-model="store.query" />
+</template>
